5a1538e9005f34cfb4aa2944c17168423e441947
ninonesix <50695635+ninonesix@users.noreply.github.com>
2019-12-03 17:04:10 +0700

    Add files via upload
    update paddle versus ball collision

--- ball.cpp
+++ ball.cpp
@@ -12,12 +12,33 @@ ball::ball()
 void ball::Update(sf::RenderWindow* window)
 {
 	float n = -1; // khai bao n = -1
+	sf::Vector2f origin;
 
-	// neu ball va cham voi _player1, _player2
-	if (this->checkCollision(this->_player1) || this->getPosition().y < 0 /*|| this->checkCollision(this->_player2)*/) 
+	// neu ball va cham voi _player1
+	if (this->checkCollision(this->_player1)) // neu banh va cham voi paddle
 	{
-		n -= 0.02f; // giam n di 0.02
-		this->_velocity.y *= n; // nhan _velocity.y voi n
+		int height_distance = this->getPosition().y + this->getGlobalBounds().height - this->_player1->getPosition().y; // tinh vi tri tuong doi cua mep duong trai banh va mep tren thanh truot
+		if (height_distance == 0) // neu mep duoi banh cham mep duoi thanh truot
+		{
+			n -= 0.02f; // giam n di 0.02
+			this->_velocity.y *= n; // nhan _velocity.y voi n
+		}
+		else if (height_distance > 0) // neu mep duoi banh thap hon mep tren thanh truot
+		{
+			sf::Vector2f paddle_velocity = this->_player1->getVelocity(); // lay gia toc cua thanh truot 
+			if (paddle_velocity.x > 0) // neu gia toc > 0
+			{ 
+				paddle_velocity.x *= -1; // tra ve gia tri am
+			}
+			n += paddle_velocity.x; // giam n di mot luong bang voi gia toc cua thanh truot 
+			this->_velocity.x *= n; // nhan _velocity.x voi n
+		}
+	}
+
+	// neu ball va cham voi tuong tren
+	if (this->getPosition().y < 0)
+	{
+		this->_velocity.y *= -1;
 	}
 
 	// neu ball va cham voi thanh` trai hoac phai
@@ -43,6 +64,7 @@ void ball::Reset(sf::RenderWindow* window)
 	this->_player1->setPosition((window->getSize().x / 2), (window->getSize().y) - 32); // dat vi tri player1 ve giua thanh` duoi
 
 	this->setPosition(window->getSize().x / 2, window->getSize().y / 2); // dat vi tri ball ve giua window
+	this->setOrigin(0, 0);
 	this->_velocity.x = 1.0f; // gan _velocity.x ve lai 1
 	this->_velocity.y = 0.75f; // gan _velocity.y ve lai 0.75
 }
